6956ec750ff200d70e38fc3fde98c8f0e4f34da1
Sayrealll <167739964+Sayrealll@users.noreply.github.com>
2026-09-12 18:48:20 +0800

    Update main.client.lua

--- main.client.lua
+++ main.client.lua
@@ -79,7 +79,7 @@ Window:Tag({
 	Color = "ElementBackground",
 })
 
-Window:Tab({
+local Tabdc = Window:Tab({
 	Title = "DISCORD",
 	Icon = "discord",
 })
@@ -99,32 +99,32 @@ local Tab2 = Window:Tab({
 	Icon = "discord",
 })
 
-Window:Tab({
+local Tab3 = Window:Tab({
 	Title = "PETS",
 	Icon = "discord",
 })
 
-Window:Tab({
+local Tab4 = Window:Tab({
 	Title = "RIFT",
 	Icon = "discord",
 })
 
-Window:Tab({
+local Tab5 = Window:Tab({
 	Title = "CONTEST",
 	Icon = "sword",
 })
 
-Window:Tab({
+local Tab6 = Window:Tab({
 	Title = "FUSE",
 	Icon = "discord",
 })
 
-Window:Tab({
+local Tab7 = Window:Tab({
 	Title = "WEBHOOK",
 	Icon = "discord",
 })
 
-Window:Tab({
+local Tab8 = Window:Tab({
 	Title = "SETTINGS",
 	Icon = "settings",
 })
@@ -184,6 +184,99 @@ VStackRight:Button({
 	end,
 })
 ]]
+
+
+
+-- ============================================================
+-- ============================================================
+-- ============================================================
+-- ============================================================
+-- ============================================================
+-- ============================================================
+-- ============================================================
+-- ============================================================
+-- ============================================================
+
+
+	local InviteCode = "ftgs-development-hub-1300692552005189632"
+	local DiscordAPI = "https://discord.gg/Ev8k5RAU3" .. InviteCode .. "?with_counts=true&with_expiration=true"
+
+	local Response = WindUI.cloneref(game:GetService("HttpService"))
+		:JSONDecode(WindUI.Creator.Request and WindUI.Creator.Request({
+			Url = DiscordAPI,
+			Method = "GET",
+			Headers = {
+				["User-Agent"] = "WindUI/Example",
+				["Accept"] = "application/json",
+			},
+		}).Body or "{}")
+
+	local Tabdc = OtherSection:Tab({
+		Title = "Discord",
+		Border = true,
+	})
+
+	if Response and Response.guild then
+		DiscordTab:Section({
+			Title = "Join our Discord server!",
+			TextSize = 20,
+		})
+		local DiscordServerParagraph = DiscordTab:Paragraph({
+			Title = tostring(Response.guild.name),
+			Desc = tostring(Response.guild.description),
+			Image = "https://cdn.discordapp.com/icons/"
+				.. Response.guild.id
+				.. "/"
+				.. Response.guild.icon
+				.. ".png?size=1024",
+			Thumbnail = "https://cdn.discordapp.com/banners/1300692552005189632/35981388401406a4b7dffd6f447a64c4.png?size=512",
+			ImageSize = 48,
+			Buttons = {
+				{
+					Title = "Copy link",
+					Icon = "link",
+					Callback = function()
+						setclipboard("https://discord.gg/" .. InviteCode)
+					end,
+				},
+			},
+		})
+	elseif RunService:IsStudio() or not writefile then
+		DiscordTab:Paragraph({
+			Title = "Discord API is not available in Studio mode.",
+			TextSize = 20,
+			Justify = "Center",
+			Image = "solar:info-circle-bold",
+			Color = "Red",
+			Buttons = {
+				{
+					Title = "Get/Copy Invite Link",
+					Icon = "link",
+					Callback = function()
+						if setclipboard then
+							setclipboard("https://discord.gg/" .. InviteCode)
+						else
+							WindUI:Notify({
+								Title = "Discord Invite Link",
+								Content = "https://discord.gg/" .. InviteCode,
+							})
+						end
+					end,
+				},
+			},
+		})
+	else
+		DiscordTab:Paragraph({
+			Title = "Failed to fetch Discord server info.",
+			TextSize = 20,
+			Justify = "Center",
+			Image = "solar:info-circle-bold",
+			Color = "Red",
+		})
+	end
+end
+
+
 local Section = Tab1:Section({
 	Title = "Auto Steal",
 	Box = true,
