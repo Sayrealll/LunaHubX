d454c6f278c2c6b7637e5e6772e369741a794b8c
Sayrealll <167739964+Sayrealll@users.noreply.github.com>
2026-09-15 20:39:41 +0800

    Update LunaHub.lua

--- LunaHub.lua
+++ LunaHub.lua
@@ -3,7 +3,7 @@ local HttpGet = game.HttpGet
 local GameId: number = game.GameId
 
 local Games: {[number]: string} = loadstring(
-  HttpGet(game, "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua")
+  HttpGet(game, "https://raw.githubusercontent.com/Sayrealll/LunaHubX/refs/heads/main/loader.lua")
 )() :: any
 
 local URL: string? = Games[GameId]
