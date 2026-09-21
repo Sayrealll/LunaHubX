17b1245b6b1bddfe1507c5066f4834fd63522963
Sayrealll <167739964+Sayrealll@users.noreply.github.com>
2026-09-22 00:42:00 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 local Games = {
     [113290951185459] = "https://raw.githubusercontent.com/Sayrealll/LunaHubX/refs/heads/main/animedice", -- ANIME DICE
     [124216119978534] = "https://raw.githubusercontent.com/Sayrealll/LunaHubX/refs/heads/main/RAP", -- RIDE A PET
-    [10563114921] = "https://raw.githubusercontent.com/Sayrealll/LunaHubX/refs/heads/main/sae.lua", -- Steal An Egg NEW UI
+    [10563114921] = "https://raw.githubusercontent.com/Sayrealll/LunaHubX/refs/heads/main/stealanegg.lua", -- Steal An Egg NEW UI
 
 }
 
