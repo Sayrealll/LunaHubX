6793c005246b510d2d79d25b47e3a1867fcb77db
Sayrealll <167739964+Sayrealll@users.noreply.github.com>
2026-09-26 23:21:34 +0800

    Delete loader.lua

--- loader.lua
+++
@@ -1,9 +0,0 @@
-local Games = {
-    
-    [113290951185459] = "https://raw.githubusercontent.com/Sayrealll/LunaHubX/refs/heads/main/animedice.lua", -- ANIME DICE
-    [124216119978534] = "https://raw.githubusercontent.com/Sayrealll/LunaHubX/refs/heads/main/rap.lua", -- RIDE A PET
-    [10563114921] = "https://raw.githubusercontent.com/Sayrealll/LunaHubX/refs/heads/main/stealanegg.lua", -- Steal An Egg NEW UI
-
-}
-
-return Games
